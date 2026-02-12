013517d9fcb57fb35c551ee2eec4c28640ce00db
sherryzhanglx-design <sherryzhanglx@gmail.com>
2026-02-11 17:51:19 -0800

    Update email sender format in sendEmail method

--- server/_core/magicLink.ts
+++ server/_core/magicLink.ts
@@ -104,7 +104,7 @@ class MagicLinkService {
     const resend = this.getResend();
 
     const { error } = await resend.emails.send({
-      from: "login@acosmos.app",
+      from: "A.Cosmos <login@acosmos.app>",
       to: email,
       subject: "Sign in to A.Cosmos",
       html: this.buildEmailHtml(magicLinkUrl),
